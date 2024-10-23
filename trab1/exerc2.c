ca3e8331516ea1e50b0d6bbb919f360f3868af36
GabrielMarquezan <marquezan.luz@acad.ufsm.br>
2024-10-23 17:35:38 -0300

    Feat: pede as dimensões da matriz, imprime percentual de não nulos, busca elemento e soma linhas

--- trab1/exerc2.c
+++ trab1/exerc2.c
@@ -32,6 +32,41 @@ struct esparsa
 };
 typedef struct esparsa Esparsa;
 
+Lista* aloca_lista()
+{
+    Lista* lista = NULL;
+    return lista;
+}
+
+void pega_dimensoes_da_matriz(Esparsa* matriz)
+{
+    int linhas = -1, colunas = -1;
+    while(true)
+    {
+        printf("\nQuantas linhas terá a matriz esparsa?\n");
+        printf("Resposta: ");
+        scanf("%d", &linhas);
+
+        if(linhas >= 1) break;
+
+        printf("Uma matriz deve ter, ao menos, uma linha!");
+    }
+
+    while(true)
+    {
+        printf("\nQuantas colunas terá a matriz esparsa?\n");
+        printf("Resposta: ");
+        scanf("%d", &colunas);
+
+        if(colunas >= 1) break;
+
+        printf("Uma matriz deve ter, ao menos, uma coluna!");
+    }
+
+    matriz->linhas = linhas;
+    matriz->colunas = colunas;
+}
+
 void append_left(Lista** lista, int elemento, int linha, int coluna)
 {
     Lista* novo_no = (Lista*) malloc(sizeof(Lista));
@@ -42,7 +77,7 @@ void append_left(Lista** lista, int elemento, int linha, int coluna)
     *lista = novo_no;
 }
 
-bool verifica_se_posicao_preenchida(Lista* lista, int linha, int coluna)
+int verifica_se_posicao_preenchida(Lista* lista, int linha, int coluna)
 {
     if(lista == NULL) return false;
     Lista* no_atual = lista;
@@ -60,34 +95,71 @@ bool verifica_se_posicao_preenchida(Lista* lista, int linha, int coluna)
     return false;
 }
 
-void preenche_lista(Lista** lista, int tamanho_maximo)
+bool deseja_fazer_algo(char texto[])
 {
-    int elemento = -1, linha = -1, coluna = -1, resposta = -1;
-    bool posicao_preenchida = false;
+    int resposta = -1;
+    while(resposta != 1 && resposta != 0)
+    {
+        printf("\nVocê deseja %s?\n", texto);
+        printf("Sim - 1\n");
+        printf("Não - 0\n");
+        printf("Resposta: ");
+        scanf("%d", &resposta);
+    }
+    return resposta == 1 ? true : false;
+}
+
+int pega_elemento()
+{
+    int elemento = -1;
+    printf("Insira o elemento a adicionar: ");
+    scanf("%d", &elemento);
+    return elemento;
+} 
+
+int pega_linha(int linha_maxima)
+{
+    int linha = -1;
+    while(true)
+    {
+        printf("Insira a linha: ");
+        scanf("%d", &linha);
+        if(linha > linha_maxima || linha < 1) printf("\n\nLINHA INVÁLIDA!\n\n");
+        else return linha;
+    }
+}
+
+int pega_coluna(int coluna_maxima)
+{
+    int coluna = -1;
+    while(true)
+    {
+        printf("Insira a coluna: ");
+        scanf("%d", &coluna);
+        if(coluna > coluna_maxima || coluna < 1) printf("\n\nCOLUNA INVÁLIDA!\n\n");
+        else return coluna;
+    }
+}
+
+void preenche_lista(Esparsa* matriz, int tamanho_maximo)
+{
+    int elemento = -1, linha = -1, coluna = -1;
+    bool resposta = false, posicao_preenchida = false;
 
     for(int i = 0; i < tamanho_maximo; i++)
     {
         while(true)
         {
-            printf("Você deseja adicionar um elemento à lista de elementos não-nulos?\n");
-            printf("Sim - 1\n");
-            printf("Não - 0\n");
-            printf("Resposta: ");
-            scanf("%d", &resposta);
+            resposta = deseja_fazer_algo("adicionar um elemento à lista de elementos não-nulos");
+            if(!resposta) break;
 
-            if(resposta != 1 && resposta != 0) continue;
-            else if(resposta == 0) break;
+            elemento = pega_elemento();
+            linha = pega_linha(matriz->linhas);
+            coluna = pega_coluna(matriz->colunas);
 
-            printf("Insira o elemento a adicionar: ");
-            scanf("%d", &elemento);
-            printf("Insira a linha do elemento: ");
-            scanf("%d", &linha);
-            printf("Insira a coluna do elemento: ");
-            scanf("%d", &coluna);
+            posicao_preenchida = verifica_se_posicao_preenchida(matriz->prim, linha, coluna);
 
-            posicao_preenchida = verifica_se_posicao_preenchida(*lista, linha, coluna);
-
-            if(!posicao_preenchida) append_left(lista, elemento, linha, coluna);
+            if(!posicao_preenchida) append_left(&matriz->prim, elemento, linha, coluna);
             else printf("Essa posição já está preenchida!\n");
         }
 
@@ -98,7 +170,7 @@ void preenche_lista(Lista** lista, int tamanho_maximo)
 int busca_elemento_da_lista_pela_posicao(Lista* lista, int linha, int coluna)
 {
     Lista* no_atual = lista;
-    if(no_atual == NULL) return 111;
+    if(no_atual == NULL) return 0;
     
     while(true)
     {
@@ -143,9 +215,18 @@ void imprime_matriz(Esparsa* matriz)
         {
             elemento = busca_elemento_da_lista_pela_posicao(matriz->prim, i, j);
 
-            if(elemento < 10) printf("|  00%d  ", elemento);
-            else if(elemento < 100) printf("|  0%d  ", elemento);
-            else printf("|  %d  ", elemento);
+            if(elemento >= 0)
+            {
+                if(elemento < 10) printf("|  00%d  ", elemento);
+                else if(elemento < 100) printf("|  0%d  ", elemento);
+                else printf("|  %d  ", elemento);
+            }
+            else
+            {
+                if(elemento > -10) printf("| -00%d  ", (-1) * elemento);
+                else if(elemento > -100) printf("| -0%d  ", (-1) * elemento);
+                else printf("| -%d  ", (-1) * elemento);
+            }
         }
         printf("|\n");
     }
@@ -153,10 +234,68 @@ void imprime_matriz(Esparsa* matriz)
     printf("\n");
 }
 
-Lista* aloca_lista()
+void busca_elemento(Esparsa* matriz)
 {
-    Lista* lista = NULL;
-    return lista;
+    int elemento = -1, linha = -1, coluna = -1;
+    bool resposta = false;
+    while(true)
+    {
+        resposta = deseja_fazer_algo("buscar um elemento da matriz");
+        if(!resposta) break;
+
+        linha = pega_linha(matriz->linhas);
+        coluna = pega_coluna(matriz->colunas);
+
+        elemento = busca_elemento_da_lista_pela_posicao(matriz->prim, linha, coluna);
+        printf("\nO elemento na linha %d e coluna %d é %d\n\n", linha, coluna, elemento);
+    }
+}
+
+int somatorio_de_linha(Esparsa* matriz, int linha)
+{
+    int total = 0;
+    for(int i = 1; i <= matriz->colunas; i++) total += busca_elemento_da_lista_pela_posicao(matriz->prim, linha, i);
+    return total;
+}
+
+void somar_linha(Esparsa* matriz)
+{
+    bool resposta = false;
+    int linha = -1, total = 0;
+
+    while(true)
+    {
+        resposta = deseja_fazer_algo("somar os valores de uma linha");
+        if(!resposta) break;
+
+        linha = pega_linha(matriz->linhas);
+        total = somatorio_de_linha(matriz, linha);
+
+        printf("\n\nO somatório dos valores da linha %d é %d\n\n", linha, total);
+    }
+}
+
+int tamanho_da_lista(Lista* lista)
+{
+    if(lista == NULL) return 0;
+    int contador = 1;
+    Lista* no_atual = lista;
+    
+    while(true)
+    {
+        Lista* proximo_no = no_atual->prox;
+        if(proximo_no == NULL) break;
+        no_atual = proximo_no;
+        contador++;
+    }
+    return contador;
+}
+
+void imprime_percentual_de_nao_nulos(Esparsa* matriz)
+{
+    float tamanho = (float) tamanho_da_lista(matriz->prim);
+    float total = (float) (matriz->linhas * matriz->colunas);
+    printf("\n\nO percentual de elementos não-nulos na matriz é: %.2f%%\n\n",  tamanho / total * 100);
 }
 
 void desaloca_lista(Lista* lista)
@@ -175,9 +314,13 @@ void desaloca_lista(Lista* lista)
 
 int main()
 {
-    Esparsa matriz = {2, 3, aloca_lista()};
+    Esparsa matriz = {0, 0, aloca_lista()};
     Esparsa* ponteiro_pra_matriz = &matriz;
-    preenche_lista(&matriz.prim, matriz.linhas * matriz.colunas);
+    pega_dimensoes_da_matriz(ponteiro_pra_matriz);
+    preenche_lista(ponteiro_pra_matriz, matriz.linhas * matriz.colunas);
     imprime_matriz(ponteiro_pra_matriz);
+    imprime_percentual_de_nao_nulos(ponteiro_pra_matriz);
+    busca_elemento(ponteiro_pra_matriz);
+    somar_linha(ponteiro_pra_matriz);
     desaloca_lista(matriz.prim);
 }
